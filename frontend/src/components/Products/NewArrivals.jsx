694860731942f9637cb3ce3bde1054bbfe14878d
Shakhawat Hossen <shakhawathossenbahar@gmail.com>
2026-01-20 11:27:49 +0600

    new arrivals section addded

--- frontend/src/components/Products/NewArrivals.jsx
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FiChevronLeft } from "react-icons/fi";
 import { FiChevronRight } from "react-icons/fi";
 import { Link } from "react-router";
 
 const NewArrivals = () => {
+  const scrollRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(false);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(true);
+
   const newArrivals = [
     {
       _id: "1",
@@ -101,44 +108,126 @@ const NewArrivals = () => {
       ],
     },
   ];
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setStartX(e.pageX - scrollRef.current.offsetLeft);
+    setScrollLeft(scrollRef.current.scrollLeft);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDragging) return;
+    const x = e.pageX - scrollRef.current.offsetLeft;
+    const walk = x - startX;
+    scrollRef.current.scrollLeft = scrollLeft - walk;
+  };
+
+  const handleMouseUpOrLeave = (e) => {
+    setIsDragging(false);
+  };
+
+  // functions for scrolling
+  const scroll = (direction) => {
+    const scrollAmount = direction === "left" ? -405 : 405;
+    scrollRef.current.scrollBy({ left: scrollAmount, behaviour: "smooth" });
+  };
+
+  // Update Scroll Buttons
+  const updateScrollButtons = () => {
+    const container = scrollRef.current;
+
+    if (container) {
+      const leftScroll = container.scrollLeft;
+      const rightScrollable =
+        container.scrollWidth > leftScroll + container.clientWidth;
+
+      setCanScrollLeft(leftScroll > 0);
+
+      setCanScrollRight(rightScrollable);
+    }
+
+    if (!container) return;
+    console.log({
+      scrollLeft: container.scrollLeft,
+      clientWidth: container.clientWidth,
+      containerScrollWidth: container.scrollWidth,
+      offsetLeft: scrollRef.current.offsetLeft,
+    });
+  };
+
+  useEffect(() => {
+    const container = scrollRef.current;
+    if (container) {
+      container.addEventListener("scroll", updateScrollButtons);
+      updateScrollButtons();
+      return () => container.removeEventListener("scroll", updateScrollButtons);
+    }
+  }, []);
+
   return (
-    <section className="container mx-auto text-center mb-10 relative">
-      <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
-      <p className="text-lg text-gray-600 mb-8">
-        Discover the latest styles straight off the runway, freshly added to
-        keep your wardrobe on the cutting edge of fashion.
-      </p>
+    <section className="py-16 px-4 lg:px-0">
+      <div className="container mx-auto text-center mb-10 relative">
+        <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
+        <p className="text-lg text-gray-600 mb-8">
+          Discover the latest styles straight off the runway, freshly added to
+          keep your wardrobe on the cutting edge of fashion.
+        </p>
 
-      {/* Scroll Buttons */}
-      <div className="absolute right-0 bottom:-30px flex space-x-2">
-        <button className="p-2 rounded-full border border-gray-400 bg-white text-black">
-          <FiChevronLeft className="text-2xl" />
-        </button>
-        <button className="p-2 rounded-full border border-gray-400 bg-white text-black">
-          <FiChevronRight className="text-2xl" />
-        </button>
-      </div>
-
-      {/* Scrollable Content */}
-      <div className="container mx-auto overflow-x-scroll flex space-x-6 relative">
-        {newArrivals.map((product) => (
-          <div
-            key={product._id}
-            className="min-w:100% sm:min-w:50% lg:min-w:30% relative "
+        {/* Scroll Buttons */}
+        <div className="absolute right-0 top-30 sm:top-20 lg:top-17 flex space-x-2">
+          <button
+            onClick={() => scroll("left")}
+            disabled={!canScrollLeft}
+            className={`p-2 rounded-full border border-gray-400 ${
+              canScrollLeft
+                ? "bg-white text-black"
+                : "bg-gray-200 text-gray-400 cursor-not-allowed"
+            }`}
           >
-            <img
-              src={product.image[0]?.url}
-              alt={product.image[0]?.altText || product.name}
-              className="w-full object-cover rounded-lg"
-            />
-            <div className="absolute bottom-0 left-0 right-0 backdrop-blur-md text-white p-4 rounded-b-lg">
-              <Link to={`/product/${product._id}`} className="block">
-                <h4 className="font-medium">{product.name}</h4>
-                <p className="mt-1">{product.price}</p>
-              </Link>
+            <FiChevronLeft className="text-2xl" />
+          </button>
+
+          <button
+            onClick={() => scroll("right")}
+            className={`p-2 rounded-full border border-gray-400 ${
+              canScrollRight
+                ? "bg-white text-black"
+                : "bg-gray-200 text-gray-400 cursor-not-allowed"
+            }`}
+          >
+            <FiChevronRight className="text-2xl" />
+          </button>
+        </div>
+
+        {/* Scrollable Content */}
+        <div
+          ref={scrollRef}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUpOrLeave}
+          onMouseLeave={handleMouseUpOrLeave}
+          className={`container mx-auto overflow-x-scroll flex space-x-6 relative ${isDragging ? "cursor-grabbing" : "cursor-grab"}`}
+        >
+          {newArrivals.map((product) => (
+            <div
+              key={product._id}
+              className="min-w-full sm:min-w-[50%] lg:min-w-[28%] relative "
+            >
+              <img
+                src={product.image[0]?.url}
+                alt={product.image[0]?.altText || product.name}
+                className="w-full h-125 object-cover rounded-lg"
+                draggable="false"
+              />
+              <div className="absolute bottom-0 left-0 right-0 backdrop-blur-md text-white p-4 rounded-b-lg">
+                <Link to={`/product/${product._id}`} className="block">
+                  <h4 className="font-medium">{product.name}</h4>
+                  <p className="mt-1">${product.price}</p>
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
