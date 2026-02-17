b67b7328508bb937f8aeeacd8e655b1b94168a60
Shakhawat Hossen <shakhawathossenbahar@gmail.com>
2026-02-17 23:36:52 +0600

    changed

--- frontend/src/components/Products/ProductGrid.jsx
+++ frontend/src/components/Products/ProductGrid.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React from "react";
+import { Link } from "react-router";
 
-const ProductGrid = ({similarProducts}) => {
-
-  // Adding commit
+const ProductGrid = ({ similarProducts }) => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
       {similarProducts.map((product, index) => (
-        <Link 
-        key={index} 
-        to={`/product/${product._id}`} className='block'>
+        <Link key={index} to={`/product/${product._id}`} className="block">
           <div className="bg-white p-4 rounded-lg">
             <div className="w-full h-96 mb-4">
-              <img 
-              src={product.images[0].url} 
-              alt={product.images[0].altText}  />
+              <img
+                src={product.images[0].url}
+                alt={product.images[0].altText}
+              />
             </div>
           </div>
         </Link>
