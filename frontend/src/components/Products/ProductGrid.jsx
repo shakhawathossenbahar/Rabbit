c87c751820ef779f198c8ad6187859d4dc976d7d
Shakhawat Hossen <shakhawathossenbahar@gmail.com>
2026-02-19 21:21:21 +0600

    changed

--- frontend/src/components/Products/ProductGrid.jsx
+++ frontend/src/components/Products/ProductGrid.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router";
 
 const ProductGrid = ({ similarProducts }) => {
 
-  // adding comment
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
       {similarProducts.map((product, index) => (
