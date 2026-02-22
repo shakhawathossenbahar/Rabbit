31a88e1e32054adc31fc51bced2666c17c305219
Shakhawat Hossen <shakhawathossenbahar@gmail.com>
2026-02-22 19:49:00 +0600

    search filter added

--- frontend/src/components/Products/FilterSidebar.jsx
+++ frontend/src/components/Products/FilterSidebar.jsx
@@ -73,6 +73,25 @@ const FilterSidebar = () => {
     setPriceRange([0, params.maxPrice || 100]);
   }, [searchParams]);
 
+  const handleFilterChange = (e) => {
+    const { name, value, checked, type } = e.target;
+    console.log({ name, value, checked, type });
+
+    let newFilters = { ...filters };
+
+    if (type === "checkbox") {
+      if (checked) {
+        newFilters[name] = [...(newFilters[name] || []), value];
+      } else {
+        newFilters[name] = newFilters[name].filter((item) => item !== value);
+      }
+    } else {
+      newFilters[name] = value;
+    }
+    setFilters(newFilters);
+    console.log(newFilters);
+  };
+
   return (
     <div className="p-4">
       <h3 className="text-xl font-medium text-gray-80 mb-4">Filter</h3>
@@ -85,6 +104,8 @@ const FilterSidebar = () => {
             <input
               type="radio"
               name="category"
+              value={category}
+              onChange={handleFilterChange}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
             />
             <span className="text-gray-700">{category}</span>
@@ -100,6 +121,8 @@ const FilterSidebar = () => {
             <input
               type="radio"
               name="gender"
+              value={gender}
+              onChange={handleFilterChange}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
             />
             <span className="text-gray-700">{gender}</span>
@@ -115,6 +138,8 @@ const FilterSidebar = () => {
             <button
               key={color}
               name="color"
+              value={color}
+              onChange={handleFilterChange}
               className="w-8 h-8 rounded-full border border-gray-300 cursor-pointer transition hover:scale-105"
               style={{ backgroundColor: color.toLowerCase() }}
             ></button>
@@ -130,11 +155,65 @@ const FilterSidebar = () => {
             <input
               type="checkbox"
               name="size"
+              value={size}
+              onChange={handleFilterChange}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
             />
             <span className="text-gray-700">{size}</span>
           </div>
         ))}
+      </div>
+
+      {/* Materials Filter */}
+      <div className="mb-6">
+        <label className="block text-gray-600 font-medum mb-2">Materials</label>
+        {materials.map((material) => (
+          <div key={material} className="flex items-center mb-1">
+            <input
+              type="checkbox"
+              name="material"
+              value={material}
+              onChange={handleFilterChange}
+              className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+            />
+            <span className="text-gray-700">{material}</span>
+          </div>
+        ))}
+      </div>
+
+      {/* Brand Filter */}
+      <div className="mb-6">
+        <label className="block text-gray-600 font-medum mb-2">Brand</label>
+        {brands.map((brand) => (
+          <div key={brand} className="flex items-center mb-1">
+            <input
+              type="checkbox"
+              name="brand"
+              value={brand}
+              onChange={handleFilterChange}
+              className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+            />
+            <span className="text-gray-700">{brand}</span>
+          </div>
+        ))}
+      </div>
+
+      {/* Price Range Filter */}
+      <div className="mb-8">
+        <label className="block text-gray-600 font-medium mb-2">
+          Price Range
+        </label>
+        <input
+          type="range"
+          name="priceRange"
+          min={0}
+          max={100}
+          className="w-full h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer"
+        />
+        <div className="flex justify-between text-gray-600 mt-2">
+          <span>$0</span>
+          <span>${priceRange[1]}</span>
+        </div>
       </div>
     </div>
   );
