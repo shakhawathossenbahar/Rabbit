8ed0b2a28e1a9dd017ae9e2fccc20df6551b301b
Shakhawat Hossen <shakhawathossenbahar@gmail.com>
2026-02-28 16:31:39 +0600

    order confirmation page added

--- frontend/src/components/Layout/CardDrawer.jsx
+++ frontend/src/components/Layout/CardDrawer.jsx
@@ -8,8 +8,9 @@ const CardDrawer = ({ drawerOpen, toggleCardDrawer }) => {
 
   const handleCheckout = () => {
     navigate("/checkout");
+    toggleCardDrawer();
   };
-  
+
   return (
     <div
       className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem]  h-full bg-white shadow-lg transform transition-transform duration-500 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}
