4460fb3da7b9352419b70f423c4b8bb858aa429a
Shakhawat Hossen <shakhawathossenbahar@gmail.com>
2026-05-09 23:53:44 +0600

    feat(backend): add user authentication APIs

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.js");
+
+// Middleware to protect routes
+const protect = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    try {
+
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.user.id).select("-password"); // Exclude password
+      next();
+
+    } catch (error) {
+      console.error("Token verification failed", error);
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    res.status(401).json({ message: "Not authorizes, no token provided" })
+  }
+}
+
+module.exports = { protect };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const {protect} = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
@@ -48,5 +49,61 @@ router.post("/register", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+
+// @route POST /api/user/login
+// @desc Authenticate user
+// @access Public
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+
+    // Find user by email
+    let user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid Credentials" });
+
+    // Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    // Sign and return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw err;
+
+        // Send the user and token in response
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        })
+      });
+
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+})
+
+
+// @route GET /api/users/profile
+// @desc Get logged-in user's profile (Protected route)
+// @access Private
+
+router.get("/profile", protect, async (req, res) => {
+  res.json(req.user);
+})
+
 
 module.exports = router;
